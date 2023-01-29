f739234d87cc31a9625e8b1435a5cc61aae9f058
lkjhgfdsa99A <123891531+lkjhgfdsa99A@users.noreply.github.com>
2023-01-29 12:06:03 -0500

    Add files via upload

---
+++ FIRBASE_WORKING_DRFT_1/FIRBASE_WORKING_DRFT_1.ino
@@ -0,0 +1,68 @@
+#include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+#define WIFI_SSID "Anushil's iPhone"
+#define WIFI_PASSWORD "praveen41"
+#define API_KEY "AIzaSyAp0Xc9qDeM5ndhXxxtm4p6nGiUOdD0MEM"
+
+
+#define DATABASE_URL "https://shack1-7f8c6-default-rtdb.firebaseio.com/m" 
+
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+long auth_code = 96417223;
+
+bool signupOK = false;
+
+void setup(){
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+  config.token_status_callback = tokenStatusCallback;
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop(){
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    if (Firebase.RTDB.setInt(&fbdo, "PATIENT_XERO", auth_code)){
+      Serial.println("TRANSMITTED SUCCESSFULLY");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+  }
+}
